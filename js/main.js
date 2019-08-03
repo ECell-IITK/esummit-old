109165166d5bdf6ba643b90de236ef189df1022b
Ayush Jain <ayushjain9501@gmail.com>
2019-08-03 12:28:38 +0530

    svg changes to what's esummit (#27)
    Improvement: Changed Background

--- js/main.js
+++ js/main.js
@@ -357,3 +357,25 @@
     }
   });
 })(jQuery);
+
+function setSize() {
+  var clientHeight = document.getElementById('intro').clientHeight;
+  console.log("client height is " + clientHeight);
+  var svgElement = document.getElementsByTagName("svg")[0];
+  svgElement.style.height = clientHeight.toString() + "px";
+}
+
+function animateWithRandomNumber(myClass, from, to) {
+  TweenLite.fromTo(myClass, Math.floor((Math.random() * 20) + 1), { y: from }, {
+    y: to,
+    onComplete: animateWithRandomNumber,
+    onCompleteParams: [myClass, from, to],
+    ease: Linear.easeNone
+  });
+}
+
+const itemsDown = [".light4", ".light5", ".light6", ".light7", ".light8", ".light11", ".light12", ".light13", ".light14", ".light15", ".light16"]
+  .forEach(e => animateWithRandomNumber(e, -1080, 1080))
+const itemsUp = [".light1", ".light2", ".light3", ".light9", ".light10", ".light17"]
+  .forEach(e => animateWithRandomNumber(e, 1080, -1080))
+setSize();
