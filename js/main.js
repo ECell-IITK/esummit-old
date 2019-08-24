41b6161eb578a73cbb3e39e8d3aeeb82c53feee4
Shivam Goyal <1998.goyal.shivam@gmail.com>
2019-08-24 11:29:44 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -371,3 +371,20 @@ const itemsDown = [".light4", ".light5", ".light6", ".light7", ".light8", ".ligh
 const itemsUp = [".light1", ".light2", ".light3", ".light9", ".light10", ".light17"]
   .forEach(e => animateWithRandomNumber(e, 1080, -1080))
 setSize();
+
+new skrolr("speaker-slider1", {
+  waitTime: 2000,
+  moveTime: 750,
+  numWide: [
+    [0, 500, 1], // width of parent is 0-499px, show 1 <li> element
+    [500, 750, 2],
+    [750, 1000, 3],
+    [1000, 1250, 4],
+    [1250, 1500, 5],
+    [1500, 1750, 6],
+    [1750, , 7] // width of parent is at least (no maximum) 1750px, show 7 <li> elements
+  ],
+  size: "100% 300px", // width then height
+  arrows: true,
+  buttons: false
+});
